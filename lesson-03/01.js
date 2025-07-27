07ae5f64a1c25646a14627865cdf664fd2a0637a
aibolat.dev <imanaibolat@gmail.com>
2025-07-27 18:11:46 +0500

    calculateAvarage

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,10 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+// Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
+
+function calculateAverage(parameter1, parameter2) {
+  const average = (parameter1+ parameter2)/2;
+  return average;
+}
+
+console.log(calculateAverage(2,3));
