28873c271f315c1da67985f8d3bb2f662cbf709d
aibolat.dev <imanaibolat@gmail.com>
2025-08-10 19:43:24 +0500

    unique arrays

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -1,5 +1,7 @@
 /*
-Напишите функцию `findUniqueElements`, которая принимает массив и возвращает новый массив, содержащий только уникальные элементы из исходного массива.
+Напишите функцию `findUniqueElements`, 
+которая принимает массив и возвращает новый массив, 
+содержащий только уникальные элементы из исходного массива.
 
 **Входные данные:**
 
@@ -12,7 +14,37 @@
 **Пример использования:**
 `findUniqueElements([1, 2, 3, 2, 1, 4])` вернёт `[1, 2, 3, 4]`.
 
-Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
+Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. 
+Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+  let uniqueNumbers = [];
+  
+  for(let i = 0; i<array.length; i++){
+    
+    let current = array[i];
+    let isUnique = true;
+    
+    for(let j = 0; j<uniqueNumbers.length; j++){
+      if(uniqueNumbers[j] === current){
+        isUnique = false;
+        break;
+      }
+    }
+    
+     if(isUnique){
+      uniqueNumbers.push(current);
+    }
+    
+  }
+  
+  return uniqueNumbers;
+ }
+
+
+console.log(findUniqueElements([1, 2, 3, 2, 1, 4]));
+
+
+
+
